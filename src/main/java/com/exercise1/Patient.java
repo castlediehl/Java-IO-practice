50d770ff77694a5478698d7abb170889d6327400
Castle Diehl <castle_diehl@hcl.com>
2021-07-10 23:55:24 -0500

    refactoring

--- src/main/java/com/exercise1/Patient.java
+++ src/main/java/com/exercise1/Patient.java
@@ -1,45 +1,51 @@
 package com.exercise1;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.io.Writer;
+import java.util.stream.Stream;
 
 /**
  * 
  * @author castle_diehl
  * Patient class implements the java Serializable interface
- * Method getInfo() prints patient's info to the console
+ *
  */
 
 public class Patient implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3071961050855431704L;
 	// Patient Info
 	String name;
 	int age;
-	static transient String hairColor;
+	String hairColor;
+	transient int SSN;
 	
 	// Constructor
-	public Patient(String name, int age, String hairColor) {
+	public Patient(String name, int age, String hairColor, int SSN) {
 		this.name=name;
 		this.age=age;
 		this.hairColor=hairColor;
+		this.SSN = SSN;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [name=" + name + ", age=" + age + ", haircolor=" + hairColor + ", Social= " + SSN + "]";
 	}
 	
-	// getInfo method
-	public void getInfo() throws IOException {
-		
-		// Data Input Stream
-		DataInputStream dis = new DataInputStream(System.in);
-		
-		// Get user input
-		System.out.println("Enter patient's name: ");
-		String name = dis.readLine();
-		System.out.println("Enter patient's age: ");
-		int age = Integer.parseInt(dis.readLine());
-		System.out.println("Enter patient's haircolor: ");
-		String hairColor = dis.readLine();
-		
-		// Print output
-		System.out.format("Your patient's name is %s, age %d, and haircolor is %s", name, age, hairColor);
-	}
+	
+	
+	
 }
--- src/main/java/com/exercise1/TestPatient.java
+++ src/main/java/com/exercise1/TestPatient.java
@@ -1,6 +1,13 @@
 package com.exercise1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
 /**
  * 
@@ -11,14 +18,39 @@ import java.io.IOException;
 
 public class TestPatient {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// New Patient
-		Patient p1 = new Patient(null, 0, null);
-		// Get Patient Info
-		p1.getInfo();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		
+		// Get user input
+		try {			
+				System.out.println("Enter patient's name: ");
+				String name;
+				name = br.readLine();
+				System.out.println("Enter patient's age: ");
+				int age = Integer.parseInt(br.readLine());
+				System.out.println("Enter patient's haircolor: ");
+				String hairColor = br.readLine();
+				System.out.println("Enter patient's social security number: ");
+				int SSN = Integer.parseInt(br.readLine());
+			
+			// New Patient
+			Patient p1 = new Patient(name, age, hairColor, SSN);
+			
+			FileOutputStream patientsFiles = new FileOutputStream("listOfPatients.txt");
+			ObjectOutputStream objectPatient = new ObjectOutputStream(patientsFiles);
+			objectPatient.writeObject(p1);
+			patientsFiles.close();
+			objectPatient.close();
+			br.close();
+			
+			} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
